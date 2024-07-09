9b1f96d1a422bb88d85d4a294debd7f6013f6835
Rohit Gupta <rohitgupta5533@gmail.com>
2024-07-09 23:28:45 +1000

    model trainer code completed

--- sensor/entity/artifact_entity.py
+++ sensor/entity/artifact_entity.py
@@ -10,14 +10,18 @@ class DataIngestionArtifact:
 class DataValidationArtifact:
     report_file_path:str
 
-
+@dataclass   
 class DataTransformationArtifact:
     transform_object_path:str
     transformed_train_path:str
     transformed_test_path:str
     target_encoder_path:str
 
-class ModelTrainerArtifact:...
+@dataclass   
+class ModelTrainerArtifact:
+    model_path:str 
+    f1_train_score:float 
+    f1_test_score:float
   
 class ModelEvaluationArtifact:...
    
