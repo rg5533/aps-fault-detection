8836867f7eecda2b29970331f303179da88ab105
Rohit Gupta <rohitgupta5533@gmail.com>
2024-07-25 18:04:51 +1000

    complted predictor functionality

--- setup.py
+++ setup.py
@@ -20,8 +20,8 @@ def get_requirements()->List[str]:
 setup(
     name="sensor",
     version="0.0.2",
-    author="ineuron",
-    author_email="avnish@ineuron.ai",
+    author="Rohit",
+    author_email="rohitgupta5533@gmail.com",
     packages = find_packages(),
     install_requires=get_requirements(),
 )
